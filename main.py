c0f44b35238ee4b15a403cb6953bfb37a9358fd7
Juxhinb7 <juxhinbazelli@gmail.com>
2023-02-22 21:23:03 +0100

    Added testing functionality

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
 from ann import NeuralNetwork
-from load_iris import load_iris
+from load_iris import load_irisdataset
 
-inputs, targets = load_iris()
+# load the training data of inputs and targets
+inputs, targets = load_irisdataset(mode="training")
 
+# initialize the neural network with input, hidden, output layers and learning rate
 nn = NeuralNetwork(4, 4, 3, 0.1)
 
+# training time
 epoch = 100
 
 for i in range(epoch):
     for j in range(len(inputs)):
         nn.train(inputs[j], targets[j])
 
-print(nn.query([7.5, 3.8, 6.6, 2.5]))
+
+# load the testing data of inputs and targets
+test_inputs, test_targets = load_irisdataset(mode="testing")
 
 
+# calculate the accuracy of the model
+def test(x_test, y_test):
+    normalised_pred_targets = []
+    for k in range(len(x_test)):
+        pred_y = nn.query(x_test[k])
+        max_val = max(pred_y)
+        normalised_item = []
+        for n in pred_y:
+            if n != max_val:
+                normalised_item.append(0)
+            else:
+                normalised_item.append(1)
+
+        normalised_pred_targets.append(normalised_item)
+
+    truevalues = 0
+
+    for index in range(len(normalised_pred_targets)):
+        if normalised_pred_targets[index] == y_test[index]:
+            truevalues += 1
+
+    accuracy = truevalues / len(normalised_pred_targets)
+    return accuracy
+
+
+# predict our new input
+print(nn.query([6.2, 3.3, 6.1, 2.4]))
+
+# output the accuracy of the model
+print("Model Accuracy:", round(test(test_inputs, test_targets), 2))
