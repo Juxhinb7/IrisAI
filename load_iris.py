26688b2ba5b97395cf22203d22ffc29e82fc4488
Juxhinb7 <juxhinbazelli@gmail.com>
2023-02-22 21:24:40 +0100

    Added testing functionality

--- load_iris.py
+++ load_iris.py
@@ -1,17 +1,27 @@
-def load_iris():
+def load_irisdataset(mode="training"):
     labels_dict = {
-        "Iris-setosa": [0.99, 0.1, 0.1],
-        "Iris-versicolor": [0.1, 0.99, 0.1],
-        "Iris-virginica": [0.1, 0.1, 0.99]
+        "Iris-setosa": [1, 0, 0],
+        "Iris-versicolor": [0, 1, 0],
+        "Iris-virginica": [0, 0, 1]
     }
     f_vector = []
     targets = []
-    with open("iris_dataset.txt") as file:
-        read_file = file.readlines()
-        for rf in read_file:
-            vector = rf.strip().split(",")
-            features = [float(x) for x in vector[:4]]
-            label = vector[4]
-            f_vector.append(features)
-            targets.append(labels_dict[label])
+    if mode == "training":
+        with open("iris_dataset.txt") as file:
+            read_file = file.readlines()
+            for rf in read_file:
+                vector = rf.strip().split(",")
+                features = [float(x) for x in vector[:4]]
+                label = vector[4]
+                f_vector.append(features)
+                targets.append(labels_dict[label])
+    elif mode == "testing":
+        with open("testdataset.txt") as file:
+            read_file = file.readlines()
+            for rf in read_file:
+                vector = rf.strip().split(",")
+                features = [float(x) for x in vector[:4]]
+                label = vector[4]
+                f_vector.append(features)
+                targets.append(labels_dict[label])
     return f_vector, targets
